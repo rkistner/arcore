900186671840d7a164d85b86b3d4dc2157c3ac55
Ralf Kistner <ralf.kistner@gmail.com>
2013-03-19 16:36:58 +0200

    Fix travis configuration error.

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 HARDWARE=~/Arduino/hardware
 TARGET=$HARDWARE/arcore
 mkdir -p $HARDWARE
-ln -s `pwd` $TARGET
-echo "Installed to $TARGET"
+if [[ -d "$TARGET" ]]; then
+  echo "Already installed at $TARGET"
+else
+  ln -s "$PWD" "$TARGET"
+  echo "Installed at $TARGET"
+fi
 
