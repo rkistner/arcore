8f965b74c88ddce1372d0ee73bac6e3ea966c765
Toby Cole <tub@tubs.org.uk>
2013-07-28 21:06:17 +0100

    Fixing install script for OSx

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-HARDWARE=~/Arduino/hardware
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  HARDWARE=~/Arduino/hardware
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  HARDWARE=~/Documents/Arduino/hardware  
+else
+  echo "OS not supported in install script, manually symlink or copy arcore to your Arduino IDE's hardware folder."
+  exit
+fi
 TARGET=$HARDWARE/arcore
 mkdir -p $HARDWARE
 if [[ -d "$TARGET" ]]; then
